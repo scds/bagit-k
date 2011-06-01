a486d341d05955752a1b10ac83720f9d3c4a1b29
nruest <ruestn@mcmaster.ca>
2011-06-01 15:09:15 -0400

    yes, yes, yes. only letters and underscores. rename stuff to make things happy.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ except ImportError:
 	from distutils.core import setup
 
 config = {
-	'description': 'bagit-K - Create bag for a directory of kirtas digitized books',
-	'author': 'Nick Ruest',
-	'url': 'https://github.com/ruebot',
-	'author_email': 'ruestn@mcmaster.ca',
+	'description': 'bagitk - Create bag for a directory of kirtas digitized books',
+	'author': 'Nick Ruest', 'Matt McCollow',
+	'url': 'https://github.com/scds/bagit-k',
+	'author_email': 'ruestn@mcmaster.ca', 'mccollo@mcmaster.ca',
 	'version': '0.1',
 	'install_requirements': 'bagit',
-	'name': 'bagit-K'
+	'name': 'bagitk'
 }
 
 setup(**config)
